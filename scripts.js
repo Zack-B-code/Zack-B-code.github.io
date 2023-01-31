0e9289aa6e1929a6d0d0b6b5420f7debfeeac0ac
ZkCode <106704877+Zack-B-code@users.noreply.github.com>
2023-01-31 17:02:55 +0000

    changed ball speed to 1

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const paddleHeight = grid * 5; // 80
 const maxPaddleY = canvas.height - grid - paddleHeight;
 
 var paddleSpeed = 6;
-var ballSpeed = 5;
+var ballSpeed = 1;
 
 const leftPaddle = {
   // start in the middle of the game on the left side
